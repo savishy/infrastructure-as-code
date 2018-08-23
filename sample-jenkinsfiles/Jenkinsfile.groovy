52af6583e5f25cdd5557bb09666387f68ccdf527
Vish <savishy@users.noreply.github.com>
2018-08-23 11:01:08 +0530

    Update Jenkinsfile.groovy

--- sample-jenkinsfiles/Jenkinsfile.groovy
+++ sample-jenkinsfiles/Jenkinsfile.groovy
@@ -11,15 +11,30 @@ The Jenkinsfile proceeds through various stages of the build pipeline:
 
 node('master') {
 
-  def dockerfileLoc = "${env.WORKSPACE}@script/sample-dockerfiles/"
+  // where is the application source?
   def repoUrl = "https://github.com/spring-projects/spring-petclinic"
+
+
+  // define a temporary workspace for building Docker images.
   def tempDir = pwd(tmp: true)
+
+  //where is JDK located?
   def javaHome = "/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+
+  //what is the build command for petclinic?
   def buildCmd = "./mvnw install"
+  
+  //What is the daemon address?
   def dockerServer = "tcp://127.0.0.1:2375"
 
+  //what is the path to the Dockerfile?
+  def dockerfileLoc = "${env.WORKSPACE}@script/sample-dockerfiles/"
+
+  // name of dockerfile to use, this is because the dir has multiple Dockerfiles named differently.
+  def dockerfileName = "Dockerfile.build"
+
   stage('Create Docker Workspace') {
-    sh "cp ${dockerfileLoc}/Dockerfile ${tempDir}"
+    sh "cp ${dockerfileLoc}/${dockerfileName} ${tempDir}/Dockerfile"
     dir(tempDir) {
         sh "ls"
     }
