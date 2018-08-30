e0275a3b18c1a57957421e9c0ae00f6dc39b50c4
Vish <savishy@users.noreply.github.com>
2018-08-30 12:41:13 +0530

    Create setenv-azure.ps1

---
+++ terraform-common/setenv-azure.ps1
@@ -0,0 +1,13 @@
+$Env:AZURE_CLIENT_ID='CHANGEME'
+$Env:AZURE_CLIENT_SECRET='CHANGEME'
+$Env:AZURE_TENANT_ID='CHANGEME'
+$Env:AZURE_SUBSCRIPTION_ID='CHANGEME'
+
+$Env:TF_VAR_azure_client_id=$Env:AZURE_CLIENT_ID
+$Env:TF_VAR_azure_client_secret=$Env:AZURE_CLIENT_SECRET
+$Env:TF_VAR_azure_tenant_id=$Env:AZURE_TENANT_ID
+$Env:TF_VAR_azure_subscription_id=$Env:AZURE_SUBSCRIPTION_ID
+$Env:TF_VAR_arm_client_id=$Env:AZURE_CLIENT_ID
+$Env:TF_VAR_arm_client_secret=$Env:AZURE_CLIENT_SECRET
+$Env:TF_VAR_arm_tenant_id=$Env:AZURE_TENANT_ID
+$Env:TF_VAR_arm_subscription_id=$Env:AZURE_SUBSCRIPTION_ID
