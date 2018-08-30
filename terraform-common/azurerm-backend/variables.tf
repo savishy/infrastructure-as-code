50c11d18560cd8f524bad2aaae0b33a6053d2dca
Vish <savishy@users.noreply.github.com>
2018-08-30 12:21:49 +0530

    Create variables.tf

---
+++ terraform-common/azurerm-backend/variables.tf
@@ -0,0 +1,9 @@
+# https://www.terraform.io/docs/configuration/variables.html
+variable "env" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+variable "azure_subscription_id" {}
+variable "location" { default = "north europe"}
+variable "tfstorageaccount_rg_name" { default = "tfstorage_rg" }
+variable "tfstorageaccount_name" { default = "terraformstoragetest" }
